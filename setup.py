1bb39b7d793a240107a3bd8186acc12eec79733f
Tomas Fiers <tomas.fiers@gmail.com>
2019-06-24 17:55:04 +0200

    Make a pip package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fario",
+    version="0",
+    author="Tomas Fiers",
+    author_email="tomas.fiers@gmail.com",
+    url="https://github.com/tfiers/fario",
+    packages=find_packages(),
+)
